bab1ba7cb463ebf4971dbc8b8980c6505528efb1
oyewolz <oyewoletaiwo219@gmail.com>
2026-04-06 22:47:37 -0400

    Initialize Apollo Federation Multisubgraph project with Maven setup and basic application structure

---
+++ src/main/java/com/oyewolz/apollofederationmultisubgraph/ApolloFederationMultisubgraphApplication.java
@@ -0,0 +1,13 @@
+package com.oyewolz.apollofederationmultisubgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloFederationMultisubgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloFederationMultisubgraphApplication.class, args);
+	}
+
+}
